33b9e1c0cf519fdc9f57605ef4f68eef3c05c179
Omer Meler <Omer.Meler@s.afeka.ac.il>
2025-10-14 21:57:11 +0300

    add initLocalLandIndex to load water map into the buffer

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import { connectDB } from "./config/db.js";
 import tripRoutes from "./routes/trip.route.js";
 import userRoutes from "./routes/user.route.js";
 import weatherRoutes from "./routes/weather.route.js";
+import { initLocalLandIndex } from "./services/localGeoService.js";
 import { swaggerDocs } from "./config/swagger.js";
 
+dotenv.config();
+
+// Load land polygons once on startup
+initLocalLandIndex();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
-
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use("/api/trips", tripRoutes);
